eab512cc8f263a54ae2713a3edb8e2d81a9a9471
DidierAtis <70582678+DidierAtis@users.noreply.github.com>
2020-11-20 10:45:57 -0500

    creation jeu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
+    //le jeu
+
+    class Jeu{
+
+        constructor() {
+
+            console.log("creation jeu")
+        }
+    }
+
+
+    //le serpent
+    class Serpent{
+
+        constructor() {
+            console.log("creation serpent")
+        }
+    }
+    //la pomme
+    class Pomme{
+
+        constructor() {
+
+            console.log("creation pomme")
+        }
+    }
+
+
+    var unePartie = new Jeu
 
 });
